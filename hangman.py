6b8e650998893e5b90a98da465c5d420d392fe73
Mengyue <Mengyue02@outlook.com>
2022-05-25 15:58:18 -0500

    Added descriptive comments

---
+++ hangman.py
@@ -0,0 +1,13 @@
+# Author: Mengyue Lu
+# Description: Very basic hangman game
+# Abstract: The program will start with minimal features and I will add onto it,
+#           The program will start will a life system and 5 letter word to guess.
+#           After the basics of the game is successfully implemented I will continue 
+#           to add on features and track them through git, this will be a quiz to 
+#           see if I learned enough of git, python loops and functions to create something
+#           on my own.
+# Additional features: I will implement the stick figure after the basics are laid out,
+#                      easy medium and hard modes, a separate file that will contain the 
+#                      words in three lists for each difficulty
+# Date Created: 05/25/2022
+# Date Last Modified: 05/25/2022
